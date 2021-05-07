f50f05324234aad8e2c11f6bc1145978838289f9
Andrey <andrey.kozhevnikov@devexpress.com>
2021-05-07 16:47:43 +0300

    prepare

---
+++ Classes/Solver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueenAttack.Classes {
+    class Solver {
+
+        public int Solve(int n, int k, int r_q, int c_q, List<List<int>> obstacles) {
+            return 0;
+        }
+    }
+}
--- Tests/SolverTests.cs
+++ Tests/SolverTests.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using NUnit.Framework;
+using QueenAttack.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace QueenAttack.Tests {
-    class Test {
+    [TestFixture]
+    public class SolverTests {
+        [Test]
+        public void SolveTest() {
+            //arrange
+            var s = new Solver();
+            
+            var obstacle = new List<int>();
+            obstacle.Add(1);
+            obstacle.Add(1);
+            var allList = new List<List<int>>();
+            allList.Add(obstacle);
+            //act
+            var res = s.Solve(5, 1, 3, 3, allList);
+            //assert
+            Assert.AreEqual(12, res);
+
+        }
     }
 }
