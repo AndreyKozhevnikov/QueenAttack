efef0fa4729c96a13370a59fb6d64572e9f7bde5
Andrey <andrey.kozhevnikov@devexpress.com>
2021-05-07 16:54:31 +0300

    l1

---
+++ Classes/Point.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueenAttack.Classes {
+  public  class Point {
+        public Point(int r, int c) {
+            Row = r;
+            Column = c;
+        }
+        public int Row{ get; set; }
+        public int Column{ get; set; }
+    }
+}
--- Classes/Solver.cs
+++ Classes/Solver.cs
@@ -8,7 +8,15 @@ namespace QueenAttack.Classes {
     class Solver {
 
         public int Solve(int n, int k, int r_q, int c_q, List<List<int>> obstacles) {
-            return 0;
+            var allSum = 0;
+
+            var queenPoint = new Point(r_q, c_q);
+            var l1First = new Point(r_q, 0);
+            var l1Sum = queenPoint.Column - l1First.Column;
+            allSum = allSum + l1Sum;
+
+
+            return allSum;
         }
     }
 }
